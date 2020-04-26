77ad71c502e3e48369240654cb9ceab28acc229b
Guillaume P <guillaume@guillaumepasquet.fr>
2020-04-26 10:37:51 -0400

    refactor main function

--- index.js
+++ index.js
@@ -4,8 +4,45 @@ const path = require('path');
 
 const job = process.argv[2];
 
-console.log('argv', process.argv);
-console.log('job', job);
+console.log('----')
+console.log('argv: ', process.argv);
+console.log('running job', job);
+console.log('----')
+
+function cleanString (str) {
+  const replacements = [
+    [/\n+$/, ''],
+    ['\033c', '']
+  ];
+
+  let cleanedString = str.toString();
+  for (const replacement of replacements) {
+    cleanedString = cleanedString.replace(...replacement);
+  }
+
+  return cleanedString;
+}
+
+function runCommand ({logger, command, defaultSpawnOptions}) {
+  return new Promise((res, rej) => {
+    const cmd = command;
+    cmd[2] = { ...defaultSpawnOptions, ...cmd[2] };
+    const x = Reflect.apply(spawn, null, cmd);
+
+    x.stderr.on('data', (chunk) => {
+      if (logger && logger.error) {
+        logger.error(chunk);
+      }
+    })
+    x.stdout.on('data', (chunk) => {
+      if (logger && logger.log) {
+        logger.log(chunk)
+      }
+    })
+    x.on('error', rej);
+    x.on('close', res);
+  });
+}
 
 (async () => {
 
@@ -19,18 +56,33 @@ console.log('job', job);
       const defers = [];
 
       for (const job in jobList.commands) {
-        const command = jobList.commands[job];
-        console.log('trying to spawn', command);
-        defers.push(new Promise((res, rej) => {
-          const x = Reflect.apply(spawn, null, command);
-          x.stderr.on('data', (chunk) => {
-            console.log(`[${job}]`, chunk.toString().replace(/\n$/, ''));
+        const maybeCommand = jobList.commands[job];
+        const command = Array.isArray(maybeCommand) ? maybeCommand : maybeCommand.command;
+        const pres = !Array.isArray(maybeCommand) && Array.isArray(maybeCommand.pre) ? maybeCommand.pre : [];
+
+        console.log(`[${job}]`, pres.length > 0 ? `${pres.length} pre jobs` : `No pre job`);
+
+        let i = 1;
+        for (const pre of pres) {
+          console.log(`[${job}] running pre job ${i++}`);
+          await runCommand({
+            command: pre,
+            logger: {
+              log: (...args) => console.log(`[${job}] [pre] `, ...args.map(a => cleanString(a))),
+              error: (...args) => console.error(`[${job}] [pre]`, ...args.map(a => cleanString(a))),
+            },
+            defaultSpawnOptions: jobList.defaultSpawnOptions || {},
           })
-          x.stdout.on('data', (chunk) => {
-            console.log(`[${job}]`, chunk.toString().replace(/\n$/, ''));
-          })
-          x.on('error', rej);
-          x.on('close', res);
+        } 
+
+        // console.log('trying to spawn', command);
+        defers.push(runCommand({
+          command,
+          logger: {
+            log: (...args) => console.log(`[${job}] `, ...args.map(a => cleanString(a))),
+            error: (...args) => console.error(`[${job}] `, ...args.map(a => cleanString(a))),
+          },
+          defaultSpawnOptions: jobList.defaultSpawnOptions || {},
         }));
       }
 
